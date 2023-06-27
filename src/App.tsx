24ebf5b23c98e3c461dafb6e8499dcef54580701
Sanith Garipally <sanith.garipally.19.1@mountblue.tech>
2023-06-27 13:41:55 +0530

    Generics and Restrictions React

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import './App.css';
+// import { List } from './components/generics/List';
+import { RandomNumber } from './components/restrictions/RandomNumber';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      {/* <List items={['Batman', 'Superman', 'Dead Pool']} onClick={(item) => console.log(item)}/>
+      <List items={[1,2,3]} onClick={(item) => console.log(item)}/> */}
+      {/* <List items={[
+        {
+          id: 1,
+          first: 'Bruce',
+          last: 'Wayne'
+        },
+        {
+          id: 2,
+          first: 'Tony',
+          last: 'Stark'
+        },
+        {
+          id: 3,
+          first: 'Sheldon',
+          last: 'Cooper'
+        },
+        
+      ]} onClick={(item) => console.log(item)}/> */}
+    <RandomNumber value={20} isPositive={true} />
     </div>
   );
 }
