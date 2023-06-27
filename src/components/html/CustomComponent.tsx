25d071fab97d11e6f12555e77f398c1316a0bf72
Sanith Garipally <sanith.garipally.19.1@mountblue.tech>
2023-06-27 14:37:28 +0530

    custom component Props

---
+++ src/components/html/CustomComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CustomButton } from './Button'
+
+export const CustomComponent = (props: React.ComponentProps<typeof CustomButton>) => {
+  return (
+    <div>
+        {/* 
+            Gets all props of a custom button component
+        */}
+    </div>
+  )
+}
